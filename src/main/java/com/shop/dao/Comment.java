8fc9c71fe11b4d058fad79dfe17f23ac4a25898f
eduardciobanu13 <eduardciobanu@gmail.com>
2015-07-31 17:17:26 +0300

    am adaugat ceva edy1

---
+++ src/main/java/com/shop/dao/Comment.java
@@ -0,0 +1,24 @@
+package com.shop.dao;
+
+public class Comment {
+
+	private Long id;
+	private String comment;
+	
+	//Start Getters and Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	//End Getters and Setters
+	
+	
+}
---
+++ src/main/java/com/shop/model/Command.java
@@ -0,0 +1,27 @@
+package com.shop.model;
+
+
+
+
+public class Command {
+
+	private Long id;
+	private float total;
+	
+
+	//Start Getters and Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public float getTotal() {
+		return total;
+	}
+	public void setTotal(float total) {
+		this.total = total;
+	}
+	//End Getters and Setters
+	
+}
