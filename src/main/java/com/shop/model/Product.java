5b142f8bc5a9ba3cd2134b0bb9c9901f13a47cd6
tilatbb <tilatbb@gmail.com>
2015-08-03 10:26:17 +0300

    am dat comit cu servlet-conf

--- src/main/java/com/shop/model/Product.java
+++ src/main/java/com/shop/model/Product.java
@@ -1,15 +1,10 @@
 package com.shop.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
