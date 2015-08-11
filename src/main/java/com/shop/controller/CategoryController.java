4b1cdb076898e1ef842729d83e00a4c426b9e660
iorgaionut <iorga.ionut.lucian@gmail.com>
2015-08-11 14:25:09 +0300

    version 1.0.45

--- src/main/java/com/shop/controller/CategoryController.java
+++ src/main/java/com/shop/controller/CategoryController.java
@@ -3,12 +3,10 @@ package com.shop.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.shop.model.Category;
+import com.shop.model.Subcategory;
 import com.shop.service.CategoryService;
 
 @Controller
@@ -25,16 +24,14 @@ public class CategoryController {
 
 	@Autowired
 	private CategoryService categoryService;
-	
-	
-	
-	@RequestMapping(value = "/addCategory", method = RequestMethod.POST)
-	public String addCategory(@RequestParam("nameCategory") String nameCategory) {	
+
+	@RequestMapping(value = "/addCategory")
+	public @ResponseBody Category addCategory(@RequestParam("nameCategory") String nameCategory) {	
 		Category category = new Category();
 		category.setName(nameCategory);
 		System.out.println(nameCategory);
 		categoryService.save(category);
-		return "redirect:addCategory.html";
+		return category;
 	}
 	
 	@RequestMapping(value="getCategories", method = RequestMethod.GET)
--- src/main/webapp/WEB-INF/jsp/addCategory.jsp
+++ src/main/webapp/WEB-INF/jsp/addCategory.jsp
@@ -8,35 +8,19 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Adaugare categorii</title>
 
-<style>
-	
-	.error {
-		color: #ff0000;
-		
-	}
-	.errorblock {
-		color: #000;
-		background-color: #ffEEEE;
-		border: 3px solid #ff0000;
-		padding: 8px;
-		margin 16px;
-	}
-	
-</style>
-
 </head>
 <body>
 
-	<form action="http://localhost:8080/lucru/addCategory" method="POST">
+	<form action="http://localhost:8080/lucru/addCategory" method="post">
 		<table>
 				<tr>
 					<td>nume categorie</td>
-					<td><input name="nameCategory"/></td>
+					<td><input name="nameCategory" type="text"></td>
 				
 				</tr>
 				<tr>
 					<td colspan="3">
-						<input type="submit" value="Adauga !"/>
+						<input type="submit" value="Adauga !">
 					</td>
 				</tr>
 			</table>
